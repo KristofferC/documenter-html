be82c6a2bdcd8ed2728367a647c0e7afbf24e773
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2017-10-23 06:50:11 -0400

    remove always true isdefined checks

--- lib/MDFlatten.jl
+++ lib/MDFlatten.jl
@@ -4,7 +4,8 @@ export mdflatten
 
 import Base.Markdown:
     MD, BlockQuote, Bold, Code, Header, HorizontalRule,
-    Image, Italic, LaTeX, LineBreak, Link, List, Paragraph, Table
+    Image, Italic, LaTeX, LineBreak, Link, List, Paragraph, Table,
+    Admonition, Footnote
 
 """
 Convert a Markdown object to a `String` of only text (i.e. not formatting info).
@@ -69,24 +70,16 @@ mdflatten(io, c::Code, parent) = print(io, c.code)
 # Special (inline) "node" -- due to JuliaMark's interpolations
 mdflatten(io, expr::Union{Symbol,Expr}, parent) = print(io, expr)
 
-
-# Only available on Julia 0.5.
-if isdefined(Base.Markdown, :Footnote)
-    import Base.Markdown: Footnote
-    mdflatten(io, f::Footnote, parent) = footnote(io, f.id, f.text, parent)
-    footnote(io, id, text::Void, parent) = print(io, "[$id]")
-    function footnote(io, id, text, parent)
-        print(io, "[$id]: ")
-        mdflatten(io, text, parent)
-    end
+mdflatten(io, f::Footnote, parent) = footnote(io, f.id, f.text, parent)
+footnote(io, id, text::Void, parent) = print(io, "[$id]")
+function footnote(io, id, text, parent)
+    print(io, "[$id]: ")
+    mdflatten(io, text, parent)
 end
 
-if isdefined(Base.Markdown, :Admonition)
-    import Base.Markdown: Admonition
-    function mdflatten(io, a::Admonition, parent)
-        println(io, "$(a.category): $(a.title)")
-        mdflatten(io, a.content, a)
-    end
+function mdflatten(io, a::Admonition, parent)
+    println(io, "$(a.category): $(a.title)")
+    mdflatten(io, a.content, a)
 end
 
 end
